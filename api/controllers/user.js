f80117ecca7b0bcd9d4b03f3e046d002c51681c1
Jens Petter Kåven <silvercat@lavvu.net>
2017-05-22 11:12:17 +0000

    first commit

---
+++ api/controllers/user.js
@@ -0,0 +1,26 @@
+'use strict';
+var User = require('../models/User');
+
+module.exports = {getAll, save, getOne, update, delUser};
+
+function getAll(req, res, next) {
+  res.json({ users: User.find() });
+}
+
+function save(req, res, next) {
+  res.json({ success: User.save(req.body), description: "User added"  });
+}
+
+function getOne(req, res, next) {
+  var id = req.swagger.params.id.value;
+  var user = User.find(id);
+  
+}
+
+function update(req, res, next) {
+
+}
+
+function delUser(req, res, next) {
+
+}
---
+++ config/db.js
@@ -0,0 +1,3 @@
+var mongoose = require('mongoose');
+
+module.exports = mongoose.createConnection('mongodb://localhost/fortunadb);
