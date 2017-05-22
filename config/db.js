6a97bbe3af33b84e6b196ae0f4d5b6915543c474
Jens Petter Kåven <silvercat@lavvu.net>
2017-05-22 14:58:29 +0000

    what

--- config/db.js
+++ config/db.js
@@ -9,11 +9,3 @@ mongoose.connection.on('error', function(err) {
 	console.error(`MongoDB connection error: ${err}`);
   process.exit(-1); // eslint-disable-line no-process-exit
 });
-
-<<<<<<< HEAD
-module.exports = mongoose.createConnection('mongodb://localhost/fortunadb');
-=======
-mongoose.connection.once('open', function() {
-	console.log('mongodb connection open');
-});
->>>>>>> b0bce15d0d898cc29a67717ca59c4d7ae98cff70
