3172fc2bea3e2b1baa233bc4f84025d6be87a9d6
Eskil Opdahl Nordland <eskil.nordland@gmail.com>
2017-05-22 18:23:40 +0200

    lols

--- api/controllers/Fisker.js
+++ api/controllers/Fisker.js
@@ -1,19 +1,19 @@
 'use strict';
-var User = require('../models/User');
+var Fisker = require('../models/Fisker');
 
 module.exports = {getAll, save, getOne, update, delUser};
 
 function getAll(req, res, next) {
-  res.json({ users: User.find() });
+  res.json({ users: Fisker.find() });
 }
 
 function save(req, res, next) {
-  res.json({ success: User.save(req.body), description: "User added"  });
+  res.json({ success: Fisker.save(req.body), description: "Fisker added"  });
 }
 
 function getOne(req, res, next) {
   var id = req.swagger.params.id.value;
-  var user = User.find(id);
+  var user = Fisker.find(id);
   
 }
 
