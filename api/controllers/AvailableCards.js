8885d274a5791a6d8be0dc163b5e5de9881a1dec
Jens Petter Kåven <silvercat@lavvu.net>
2017-05-26 16:42:07 +0200

    test

--- api/controllers/AvailableCards.js
+++ api/controllers/AvailableCards.js
@@ -4,12 +4,13 @@ module.exports = {GetAvailableCards};
 function GetAvailableCards(req, res) {
 	var year = req.swagger.params.year.value;
 	var month = req.swagger.params.month.value;
+	var requestDate = req.swagger.params.date.value;
 	var CardDate = function(date, availablecards) {
 		this.date = date,
 		this.availablecards = availablecards
 	};
 	var dateList = [];
-	dateList.push(new CardDate("1", "15"));
-	dateList.push(new CardDate("2", "9"));
+	dateList.push(new CardDate(1, 15));
+	dateList.push(new CardDate(2, 9));
 	res.json(dateList);
 }
