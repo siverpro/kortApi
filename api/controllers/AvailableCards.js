4690b501844c970eae498e32305d094a8a4cd1a7
Jens Petter Kåven <silvercat@lavvu.net>
2017-05-26 16:16:42 +0200

    Cards logic

---
+++ api/controllers/AvailableCards.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = {GetAvailableCards};
+
+function GetAvailableCards(req, res) {
+	var year = req.swagger.params.year.value;
+	var month = req.swagger.params.month.value;
+	var CardDate = function(date, availablecards) {
+		this.date = date,
+		this.availablecards = availablecards
+	};
+	var dateList = [];
+	dateList.push(new CardDate(1, 15));
+	dateList.push(new CardDate(2, 9));
+	res.json(dateList);
+}
